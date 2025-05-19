f4ff77c728ab13b8b3e46c988d80b205dda6edeb
DaviLealdev <198823429+DaviLealdev@users.noreply.github.com>
2025-05-18 21:39:29 -0300

    commit002

--- scripts.js
+++ scripts.js
@@ -3,3 +3,18 @@ const audio = new Audio("deixa.ogg");
 audio.play();
 console.log("funciona")
 }
+
+let bucks = document.getElementById("bucks-count")
+let level = document.getElementById("level")
+
+const leftButton = document.getElementById("left-button")
+const rightButton = document.getElementById("right-button")
+
+function goToBedRoom(){
+    let container = document.getElementById("container")
+    let comodo = document.getElementById("comodo")
+    container.style.backgroundColor = 'pink';
+    comodo.innerHTML = 'Quarto'
+    rightButton.style.display = 'none'
+    
+}
